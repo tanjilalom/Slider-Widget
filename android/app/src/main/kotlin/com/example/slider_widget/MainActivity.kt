8e2f04ae2f082f8313faf6dc345740ce5014f204
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-03-17 06:58:06 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/slider_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.slider_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget  {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _value = 30.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Slider Widget"),
+        backgroundColor: Colors.deepPurple[100],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("This is an Slider", style: TextStyle(fontSize: _value),),
+            SizedBox(height: 50,),
+            Slider(
+                value: _value,
+                min: 20,
+                max: 100,
+                label: _value.round().toString(),
+                divisions: 4,
+                onChanged: (value) {
+                  setState(() {
+                    _value = value;
+                    print(value);
+                  });
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
